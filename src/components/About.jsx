72e6972ddcacf848dc8420de7f4f60a39952cd98
Denis Kulik <kulikdenis1996x@gmail.com>
2023-03-13 19:25:05 +0700

    add links to others pages

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,11 @@
+import { Link } from 'react-router-dom';
+
 export default function About() {
     return (
-        <h1>About</h1>
+        <>
+            <h1>About</h1>
+            <Link to="/react-router">Go to Home</Link>
+            <Link to="/react-router/contacts">Go to Contacts</Link>
+        </>
     );
 }
