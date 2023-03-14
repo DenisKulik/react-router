c5bf139ccf2d3eab79e64004c4a01fe5f0a4b94b
Denis Kulik <kulikdenis1996x@gmail.com>
2023-03-14 19:24:17 +0700

    add courses data

---
+++ src/data/courses.js
@@ -0,0 +1,24 @@
+const courses = [
+    {
+        title: 'Complete React Guide',
+        slug: 'react',
+        id: 257,
+    },
+    {
+        title: 'Complete JavaScript Guide',
+        slug: 'javascript',
+        id: 573,
+    },
+    {
+        title: 'Complete Node.js Guide',
+        slug: 'node-js',
+        id: 124,
+    },
+    {
+        title: 'Complete Angular Guide',
+        slug: 'angular',
+        id: 245,
+    },
+];
+
+export default courses;
