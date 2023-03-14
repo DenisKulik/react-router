56792c728ecc7785f9033a73fe8809d86112bab2
Denis Kulik <kulikdenis1996x@gmail.com>
2023-03-14 20:45:25 +0700

    add courses link styles

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -1,13 +1,18 @@
-import courses from '../data/courses';
 import { Link } from 'react-router-dom';
+import courses from '../data/courses';
 
 export default function Courses() {
     return (
         <>
             <h1>Courses</h1>
             {courses.map((course) => {
-                return <Link key={course.id}
-                             to={course.slug}>{course.title}</Link>;
+                return (
+                    <div key={course.id}>
+                        <Link to={course.slug} className="courseLink">
+                            {course.title}
+                        </Link>
+                    </div>
+                );
             })}
         </>
     );
